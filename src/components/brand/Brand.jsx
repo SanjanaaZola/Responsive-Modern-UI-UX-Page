a9cd4c4e731a057b0e820098dabccf112a9ae0cd
SanjanaaZola <zolasanjanaa@gail.com>
2021-11-30 03:19:05 +0530

    brand updated

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
 import './brand.css'
 
+import Google from '../../Assets/google.png'
+import Slack from '../../Assets/slack.png'
+import Atlassian from '../../Assets/atlassian.png'
+import Dropbox from '../../Assets/dropbox.png'
+import Shopify from '../../Assets/shopify.png'
+
 const Brand = () => {
-  return <div>Brand</div>
+  return (
+    <div className='gpt3__brand section__heading'>
+      <div>
+        <img src={Google} alt='google' />
+      </div>
+      <div>
+        <img src={Slack} alt='slack' />
+      </div>
+      <div>
+        <img src={Atlassian} alt='atlassian' />
+      </div>
+      <div>
+        <img src={Dropbox} alt='dropbox' />
+      </div>
+      <div>
+        <img src={Shopify} alt='shopify' />
+      </div>
+    </div>
+  )
 }
 
 export default Brand
